c9069b847c8fc0a08d66b06352347c68e65d00a4
pulkitagg17 <pulkitiscoding@gmail.com>
2026-07-03 16:28:43 +0530

    Permutations  done

--- Introductory Problems/Permutations.cpp
+++ Introductory Problems/Permutations.cpp
@@ -6,8 +6,15 @@ int main(){
     
     ll n;
     cin>>n;
+    if(n==1) cout<<1;
+    else if(n<=3) cout<<"NO SOLUTION";
+    else{
+        ll odd=1;
+        ll even=2;
 
-    
+        while(even<=n) cout<<even<<" ", even+=2;
+        while(odd<=n) cout<<odd<<" ", odd+=2;
+    }
 
     return 0;
 }
